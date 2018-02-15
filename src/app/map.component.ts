7ea1d91b6575b03c9efff6a1266ee6caadf80864
Denis Suvolokin <budden@list.ru>
2018-02-15 18:27:53 +0400

    add css animation for map

--- src/app/map.component.ts
+++ src/app/map.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'map',
   templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent {
   toggled:boolean=false;
